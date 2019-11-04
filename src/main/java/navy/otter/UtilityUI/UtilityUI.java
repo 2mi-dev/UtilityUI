6f1087a7b7d708dd875e51dc4a5e6898693f92c6
Moritz <moritz.mikus@web.de>
2019-11-04 15:30:24 +0100

    added permissions

--- src/main/java/navy/otter/UtilityUI/UtilityUI.java
+++ src/main/java/navy/otter/UtilityUI/UtilityUI.java
@@ -19,144 +19,164 @@ import java.util.Iterator;
 
 public class UtilityUI extends JavaPlugin implements CommandExecutor {
 
-    private HashMap<Player, Inventory> brewingMap;
+  private HashMap<Player, Inventory> brewingMap;
 
-    @Override
-    public void onEnable() {
-        getLogger().info("onEnable is called!");
-        this.getCommand("uui").setExecutor(this);
-        this.getCommand("uui").setTabCompleter(new EmptyTabCompleter());
-        this.brewingMap = new HashMap<>();
+  @Override
+  public void onEnable() {
+    getLogger().info("UtilityUI enabled");
+    this.getCommand("uui").setExecutor(this);
+    this.getCommand("uui").setTabCompleter(new EmptyTabCompleter());
+    this.brewingMap = new HashMap<>();
+  }
+
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+    if (!(sender instanceof Player)) {
+      return false;
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        if (!(sender instanceof Player)) {
-            return false;
-        }
-
-        final Player player = (Player) sender;
-        if (!player.hasPermission("")) {
-            return false;
-        }
-
-
-        Iterator<String> arg = Arrays.asList(args).iterator();
-        String option = arg.hasNext() ? arg.next() : "";
-        String verifier = arg.hasNext() ? arg.next() : "";
-
-        boolean result = false;
-
-        switch (option.toLowerCase()) {
-            case "":
-                result = false;
-                break;
-            case "debug":
-                if (player.hasPermission("UtilityUI.admin")) {
-                    result = giveDebugStick(player);
-                }
-                break;
-            case "cmd":
-                if(player.hasPermission("UtilityUI.admin")) {
-                    result = giveCommandBlock(player);
-                }
-            default:
-                player.sendMessage(ChatColor.RED + "Der eingegebene Befehl existiert nicht.");
-        }
-        return result;
+    final Player player = (Player) sender;
+    if (!player.hasPermission("")) {
+      return false;
     }
 
-    public boolean giveDebugStick (Player player) {
-        if(player != null) {
-            PlayerInventory playerinventory = player.getInventory();
-            if(checkForEmptySpace(playerinventory)) {
-                playerinventory.addItem(new ItemStack(Material.DEBUG_STICK, 1));
-                return true;
-            } else {
-                String sb = ChatColor.AQUA +
-                        "UtilityUI: " +
-                        ChatColor.RED +
-                        "Dein Inventar ist voll!";
-                player.sendMessage(sb);
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
+    Iterator<String> arg = Arrays.asList(args).iterator();
+    String option = arg.hasNext() ? arg.next() : "";
+    String verifier = arg.hasNext() ? arg.next() : "";
 
-    public boolean giveCommandBlock (Player player) {
-        if(player != null) {
-            PlayerInventory playerinventory = player.getInventory();
-            if(checkForEmptySpace(playerinventory)) {
-                playerinventory.addItem(new ItemStack(Material.COMMAND_BLOCK, 1));
-                return true;
-            } else {
-                String sb = ChatColor.AQUA +
-                        "UtilityUI: " +
-                        ChatColor.RED +
-                        "Dein Inventar ist voll!";
-                player.sendMessage(sb);
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
+    boolean result = false;
 
-    public boolean showAnvilGUI (Player player) {
-        if(player != null) {
-            Inventory anvil = Bukkit.createInventory(player, InventoryType.ANVIL);
-            return true;
-        } else {
-            return false;
+    switch (option.toLowerCase()) {
+      case "":
+        result = false;
+        break;
+      case "debug":
+        if (player.hasPermission("UtilityUI.debugstick")) {
+          result = giveDebugStick(player);
         }
+        break;
+      case "cmd":
+        if (player.hasPermission("UtilityUI.commandblock")) {
+          result = giveCommandBlock(player);
+        }
+        break;
+      case "anvil":
+        if (player.hasPermission("UtilityUI.anvil")) {
+          result = showAnvilGui(player);
+        }
+        break;
+      case "brew":
+        if (player.hasPermission("UtilityUI.brewing")) {
+          result = showBrewingGui(player);
+        }
+        break;
+      case "wb":
+        if (player.hasPermission("UtilityUI.workbench")) {
+          result = showWorkbenchGui(player);
+        }
+        break;
+      case "creative":
+        if (player.hasPermission("UtilityUI.creative")) {
+          result = showCreativeInventoryGui(player);
+        }
+        break;
+      default:
+        player.sendMessage(ChatColor.RED + "Der eingegebene Befehl existiert nicht.");
     }
+    return result;
+  }
 
-    public boolean showWorkbenchGUI (Player player) {
-        if(player != null) {
-            player.openWorkbench(null, true);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean showBrewingGUI (Player player) {
-        if(player != null) {
-            if(brewingMap.get(player) == null) {
-                Inventory brewing = Bukkit.createInventory(player, InventoryType.BREWING);
-                brewingMap.put(player, brewing);
-            } else {
-                Inventory brewing = brewingMap.get(player);
-                player.openInventory(brewing);
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean showCreativeInventoryGUI (Player player) {
-        if(player != null) {
-            Inventory creative = Bukkit.createInventory(player, InventoryType.CREATIVE);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean checkForEmptySpace(PlayerInventory inventory){
-        for (ItemStack item: inventory.getContents()) {
-            if(item == null) {
-                return true;
-            }
-        }
+  public boolean giveDebugStick(Player player) {
+    if (player != null) {
+      PlayerInventory playerinventory = player.getInventory();
+      if (checkForEmptySpace(playerinventory)) {
+        playerinventory.addItem(new ItemStack(Material.DEBUG_STICK, 1));
+        return true;
+      } else {
+        String sb = ChatColor.AQUA +
+            "UtilityUI: " +
+            ChatColor.RED +
+            "Dein Inventar ist voll!";
+        player.sendMessage(sb);
         return false;
+      }
+    } else {
+      return false;
     }
+  }
 
-    @Override
-    public void onDisable() {
-        getLogger().info("onDisable is called!");
+  public boolean giveCommandBlock(Player player) {
+    if (player != null) {
+      PlayerInventory playerinventory = player.getInventory();
+      if (checkForEmptySpace(playerinventory)) {
+        playerinventory.addItem(new ItemStack(Material.COMMAND_BLOCK, 1));
+        return true;
+      } else {
+        String sb = ChatColor.AQUA +
+            "UtilityUI: " +
+            ChatColor.RED +
+            "Dein Inventar ist voll!";
+        player.sendMessage(sb);
+        return false;
+      }
+    } else {
+      return false;
     }
+  }
+
+  public boolean showAnvilGui(Player player) {
+    if (player != null) {
+      Bukkit.createInventory(player, InventoryType.ANVIL);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean showWorkbenchGui(Player player) {
+    if (player != null) {
+      player.openWorkbench(null, true);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean showBrewingGui(Player player) {
+    if (player != null) {
+      if (brewingMap.get(player) == null) {
+        Inventory brewing = Bukkit.createInventory(player, InventoryType.BREWING);
+        brewingMap.put(player, brewing);
+      } else {
+        Inventory brewing = brewingMap.get(player);
+        player.openInventory(brewing);
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean showCreativeInventoryGui(Player player) {
+    if (player != null) {
+      Bukkit.createInventory(player, InventoryType.CREATIVE);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean checkForEmptySpace(PlayerInventory inventory) {
+    for (ItemStack item : inventory.getContents()) {
+      if (item == null) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public void onDisable() {
+    getLogger().info("UtilityUI disabled");
+  }
 }
